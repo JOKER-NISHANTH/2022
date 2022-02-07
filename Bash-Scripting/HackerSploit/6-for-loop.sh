1e63aeb2bb6c9acdc87143172bac3211e10929ef
Nishanth <innocentnishanth@gmail.com>
2022-02-07 22:49:53 +0530

    for loop

---
+++ Bash-Scripting/HackerSploit/6-for-loop.sh
@@ -0,0 +1,9 @@
+
+# for <varaiable> in $(<list>);
+# do
+#     statement
+# done
+for NAME in $(cat /d/videoTutorialFiles/2022/Bash-Scripting/HackerSploit/name.txt);
+do
+    echo "Name : $NAME"
+done
