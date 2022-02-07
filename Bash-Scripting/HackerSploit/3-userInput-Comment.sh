5ae061f04da09f248af8a4c350e2ba893a03c328
Nishanth <innocentnishanth@gmail.com>
2022-02-07 19:53:10 +0530

    userinput and comment

---
+++ Bash-Scripting/HackerSploit/3-userInput-Comment.sh
@@ -0,0 +1,8 @@
+
+# User Input
+#read -p "Enter your name : " NAME # -p prompt
+#echo "Your name is $NAME"
+
+echo "Please enter your full name : "
+read FNAME LNAME
+echo "Your name first name is $FNAME and last name is $LNAME"
