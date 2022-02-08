1dc2961f3a9a89b9533a81ccc707acb4ce98fbf4
Nishanth <innocentnishanth@gmail.com>
2022-02-08 21:23:24 +0530

    15.	Multi line comments using heredocs

--- Bash-Scripting/linuxHint/15.sh
+++ Bash-Scripting/linuxHint/15.sh
@@ -2,7 +2,5 @@
 
 <<MULTI-LINE_COMMENT
 echo "My Favorite sport is \"Kabbadi\"" > ./textFiles/fav-sport.txt
-
-
 echo "My Favorite sport is \"Foot Ball\"" >> ./textFiles/fav-sport.txt
 MULTI-LINE_COMMENT
