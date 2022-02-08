ec0b4c813781296a79163b4ae8e99a92641b5eec
Nishanth <innocentnishanth@gmail.com>
2022-02-08 21:25:18 +0530

    23.	Simple Touch Command

--- Bash-Scripting/linuxHint/23.sh
+++ Bash-Scripting/linuxHint/23.sh
@@ -3,3 +3,4 @@
 touch /home/nisha/learn/bashScript/linuxHint/textFiles/touch-file.txt
 echo "This file is create using touch command" > /home/nisha/learn/bashScript/linuxHint/textFiles/touch-file.txt
 cat /home/nisha/learn/bashScript/linuxHint/textFiles/touch-file.txt
+
