ef220bcc4706b801735a0a7436b8717135566e18
Nishanth <innocentnishanth@gmail.com>
2022-02-21 22:25:21 +0530

    Subdomains Enumurations

--- Bash-Scripting/Recon/subEnum.sh
+++ Bash-Scripting/Recon/subEnum.sh
@@ -1,8 +1,59 @@
 #!/bin/bash
+
 domain=$1
+worldlist="/root/wordlist/SecList/Discovery/DNS"
+resolvers="/root/resolvers.txt"
 
-subfinder -d $domain -o subfinder.txt
 
-assetfinder -subs-only $domain | tee assetfinder.txt
+domain_enum() {
 
-amass enum -passive -d $domain -o amassPassive.txt -config /path/config.ini
+    mkdir -p $domain $domain/sources $domain/Recon
+
+    # Passive Enum Step 1
+
+    subfinder -d $domain -o $domain/sources/subfinder.txt
+
+    assetfinder -subs-only $domain | tee $domain/sources/assetfinder.txt
+
+    amass enum -passive -d $domain -o $domain/sources/amassPassive.txt -config /path/config.ini
+
+    # Active Enum Step 2
+
+    shuffledns -d $domain -w $worldlist -r $resolvers -o $domain/sources/shuffledns.txt
+
+
+    cat $domain/sources/*.txt > $domain/sources/all.txt
+
+
+}
+domain_enum
+# ---------------------------------------------------------------------------------------------------------------------------------------
+resolving_domains() {
+
+    #  Resolve Domains Step 3
+
+    shuffledns -d $domain -w $worldlist -list $domain/sources/all.txt  -r $resolvers -o $domain/domains.txt
+
+}
+resolving_domains
+
+# ---------------------------------------------------------------------------------------------------------------------------------------
+http_prob(){
+    # Step 4
+    cat $domain/domains.txt | httpx -threads 20 -o $domain/Recon/httpx.txt
+
+}
+
+http_prob
+
+# ---------------------------------------------------------------------------------------------------------------------------------------
+
+
+
+
+# ---------------------------------------------------------------------------------------------------------------------------------------
+
+
+
+
+# ---------------------------------------------------------------------------------------------------------------------------------------
