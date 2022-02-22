bb51d3f2a566413fe872bbd38bafefd7871e32fa
Nishanth <innocentnishanth@gmail.com>
2022-02-22 09:01:43 +0530

    sub

--- Bash-Scripting/Recon/subEnum.sh
+++ Bash-Scripting/Recon/subEnum.sh
@@ -7,7 +7,7 @@ resolvers="/root/resolvers.txt"
 
 domain_enum() {
 
-    mkdir -p $domain $domain/sources $domain/Recon
+    mkdir -p $domain $domain/sources $domain/Recon $domain/Recon/nuclei
 
     # Passive Enum Step 1
 
@@ -48,11 +48,19 @@ http_prob
 
 # ---------------------------------------------------------------------------------------------------------------------------------------
 
+scanner() {
 
+    cat  $domain/Recon/httpx.txt | nuclei -t ~/.local/nuclei-templates/cves -c 50 -o  $domain/Recon/nuclei/cves.txt
+    cat  $domain/Recon/httpx.txt | nuclei -t ~/.local/nuclei-templates/vulnerabilities -c 50 -o  $domain/Recon/nuclei/vulnerabilities.txt
+    cat  $domain/Recon/httpx.txt | nuclei -t ~/.local/nuclei-templates/file -c 50 -o  $domain/Recon/nuclei/file.txt
+    cat  $domain/Recon/httpx.txt | nuclei -t ~/.local/nuclei-templates/technologies -c 50 -o  $domain/Recon/nuclei/technologies.txt
+    cat  $domain/Recon/httpx.txt | nuclei -t ~/.local/nuclei-templates/misconfiguration -c 50 -o  $domain/Recon/nuclei/misconfiguration.txt
 
+}
+
+scanner
 
 # ---------------------------------------------------------------------------------------------------------------------------------------
-
 
 
 
