2948a9d9bd4d0b9b03ae5391cfe20e2393612858
Nishanth <innocentnishanth@gmail.com>
2022-04-16 19:52:58 +0530

    initial project setup

---
+++ Portfolio/VR/portfolio/src/App.js
@@ -0,0 +1,11 @@
+
+
+function App() {
+  return (
+    <div className="flex w-screen h-screen items-center justify-center">
+      <p className="text-2xl text-blue-500">Hai there !</p>
+    </div>
+  );
+}
+
+export default App;
