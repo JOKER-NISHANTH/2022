d32bebc7b5fa006fc9921c8325e207a897e7d9e3
Nishanth <innocentnishanth@gmail.com>
2022-06-28 20:12:39 +0530

    table and dummmy data created

--- 2022-June/React/Tamil/JVL-Code/Hooks/react-hooks-useeffect-usestate/src/App.jsx
+++ 2022-June/React/Tamil/JVL-Code/Hooks/react-hooks-useeffect-usestate/src/App.jsx
@@ -1,4 +1,14 @@
+import {  useState } from "react";
+import { UserTable } from "./tables/UserTable";
+
 function App() {
+
+  const userData = [
+    {id:1,name:"Nishanth",username:"mr-black"},
+    {id:2,name:"Vaseekaran",username:"vasee"},
+    {id:3,name:"Vijaysurya",username:"surya"},
+  ]
+  const[users,setUsers] =useState(userData);
   return (
     <div className="container">
       <h1>CRUD App with Hooks</h1>
@@ -7,7 +17,8 @@ function App() {
           <h2>Add User</h2>
         </div>
         <div className="flex-large">
-          <h2>Add User</h2>
+          <h2>View User</h2>
+          <UserTable users={ users}/>
         </div>
       </div>
     </div>
