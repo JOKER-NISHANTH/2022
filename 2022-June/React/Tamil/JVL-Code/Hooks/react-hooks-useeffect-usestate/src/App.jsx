4754597b0f98c47996ea6bc567343794bafc49d7
Nishanth <innocentnishanth@gmail.com>
2022-06-28 21:45:46 +0530

    update form added

--- 2022-June/React/Tamil/JVL-Code/Hooks/react-hooks-useeffect-usestate/src/App.jsx
+++ 2022-June/React/Tamil/JVL-Code/Hooks/react-hooks-useeffect-usestate/src/App.jsx
@@ -1,11 +1,10 @@
 import {  useState } from "react";
 import AddUserForm from "./forms/AddUserForm";
+import { EditUserForm } from "./forms/EditUserForm";
 
 import { UserTable } from "./tables/UserTable";
 
 function App() {
-
-
 
   const userData = [
     { id: 1, name: "Nishanth", username: "mr-black" },
@@ -26,17 +25,48 @@ function App() {
   }
 
   const[users,setUsers] =useState(userData);
+
+  const[editing,setEditing] = useState(false)
+  const initialFormState = {
+    id: null,
+    name: '',
+    username: '',
+  };
+  const[currentUser,setCurentUSer] = useState(initialFormState)
+
+
+  const editRow = (user) => {
+    setEditing(true);
+    setCurentUSer({ id: user.id, name: user.name, username: user.username });
+  }
+
+  const updateUser = (id,updatedUser) => {
+    setEditing(false);
+    setUsers(users.map((user)=>(user.id===id?updatedUser:user)));
+  }
   return (
     <div className="container">
       <h1>CRUD App with Hooks</h1>
       <div className="flex-row">
         <div className="flex-large">
-          <h2>Add User</h2>
+          {editing ?(
+            <div>
+              <h2>Edit User Form</h2>
+              < EditUserForm
+                setEditing={setEditing}
+                currentUser={currentUser}
+                updateUser={updateUser}
+              />
+            </div>) : (<div>
+              <h2>Add User</h2>
           <AddUserForm addUser={ addUser}/>
+          </div>)
+        }
+
         </div>
         <div className="flex-large">
           <h2>View User</h2>
-          <UserTable deleteUser={deleteUser} users={ users}/>
+          <UserTable editRow={editRow} deleteUser={deleteUser} users={ users}/>
         </div>
       </div>
     </div>
