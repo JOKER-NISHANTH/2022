f43ee9fd6876b0c46e9447a5cd6782ab6b6398df
Nishanth <innocentnishanth@gmail.com>
2022-07-01 21:00:45 +0530

    JS ES6 let const

---
+++ 2022-June/JavaScript/udemy/Modern JS ES6/1-let-const.js
@@ -0,0 +1,16 @@
+let num = 10;
+console.log(num);
+
+let name = "Mr Black"
+console.log(name)
+
+const PI = 3.14
+console.log(PI)
+//! let and const not support Hoisting
+console.log(hoist);
+var hoist = "The process whereby the interpreter appears to move the declaration of functions, variables or classes to the top of their scope, prior to execution of the code."
+console.log(hoist);
+
+console.log(hoistLet);
+let hoistLet = "The process whereby the interpreter appears to move the declaration of functions, variables or classes to the top of their scope, prior to execution of the code."
+console.log(hoistLet);
