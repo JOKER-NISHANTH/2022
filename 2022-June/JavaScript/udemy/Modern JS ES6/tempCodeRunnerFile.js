dc3b5421d5917a0ac2ac189896e8e954b84725bc
Nishanth <innocentnishanth@gmail.com>
2022-07-02 11:58:14 +0530

    Arrow filter function

--- 2022-June/JavaScript/udemy/Modern JS ES6/tempCodeRunnerFile.js
+++ 2022-June/JavaScript/udemy/Modern JS ES6/tempCodeRunnerFile.js
@@ -1,7 +1,6 @@
-const arr = [1, 2, 3, 4, 5]
-let result = arr.reduce((acc, item) => {
-    console.log(`Item : ${item}`)
-    console.log(`Acc : ${acc}`)
-    return acc + item;
-},0)
+const arr = [1, 2, 3, '4', 5, '6', 7, 8, 9];
+let result = arr.filter((val) => {
+    // return val % 2 == 0;
+    return val % 2 === 0;
+})
 console.log(result)
