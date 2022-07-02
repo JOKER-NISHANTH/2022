3c8dc6c2308a4f10129f2b89eb294742eecc9fbf
Nishanth <innocentnishanth@gmail.com>
2022-07-02 09:36:52 +0530

    Files renamed

--- 2022-June/JavaScript/udemy/Modern JS ES6/script.js
+++ 2022-June/JavaScript/udemy/Modern JS ES6/script.js
@@ -1,24 +1,36 @@
 console.clear()
 
-function greeting (name){
-    console.log(`Welcome ${name}`)
+// Rest Operator
+
+const mSum = (num1,num2) => console.log(num1+num2)
+mSum(1, 2, 3, 4, 5);
+//  o/p 3 -> 1st and 2nd args only accepted reset of args are ignore
+
+/*
+ * Rest Operator
+ * ! Rest operator is like *args in Python
+ * * Its combine the n args and form the array
+ * ? Rest parameter must be last formal parameters
+ */
+// TODO : Do some research on Rest Operator
+//? Formal parameters is nothing but, Parameters in function or methods
+//? Actual parameters is Arguments in function or methods
+
+const singleStarInPython = (...args) => console.log(args)
+singleStarInPython(1, 2, 3, 4, 5); // return array
+
+
+// Sum of n numbers using REST Operator
+const nSum = (...args) => {
+    let result = 0;
+    for (const num of args) {
+    // for (const num in args) {
+        result += num
+    }
+    // return {result}
+    return result
 }
-
-// fat arrow
-
-const aGreeting1 = (name) =>{
-    console.log(`Welcome ${name}`)
-}
-
-// single args explicit return
-const aGreeting2 = name =>{
-    return `Welcome ${name}`
-}
-// single args with implicit return
-const aGreeting3 = name => `Welcome ${name}`
-
-
-greeting("Mr Black")
-aGreeting1("Mr Brown")
-console.log(aGreeting2("Mr Blue"));
-console.log(aGreeting3("Mr Red"));
+// console.log(nSum(1,2,3,4,5,6,7,8,9))
+// !-----------------------------------------------------------------
+const totalBill = (name, ...args) =>  `Hello! ${name} your bill was ${nSum(...args)}rs`
+console.log(totalBill("Mr Black",10,20,30,40))
