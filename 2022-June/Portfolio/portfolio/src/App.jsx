cf4ae11081873b521c455543202216a86e909e90
Nishanth <innocentnishanth@gmail.com>
2022-07-02 20:42:31 +0530

    all components created

---
+++ 2022-June/Portfolio/portfolio/src/App.jsx
@@ -0,0 +1,15 @@
+
+import './App.css';
+import Headers from "./components/header/Header"
+import React from 'react'
+
+const App = () => {
+  return (
+    <>
+      <Headers />
+
+    </>
+  )
+}
+
+export default App
