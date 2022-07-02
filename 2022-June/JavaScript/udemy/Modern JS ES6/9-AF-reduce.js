1eb14e1679961ea8eea787354055bbf5942b0981
Nishanth <innocentnishanth@gmail.com>
2022-07-02 11:41:43 +0530

    Reduce function

--- 2022-June/JavaScript/udemy/Modern JS ES6/9-AF-reduce.js
+++ 2022-June/JavaScript/udemy/Modern JS ES6/9-AF-reduce.js
@@ -1,5 +1,32 @@
+console.log("REDUCE Higher order function")
 // *-----------------------------------------------------------------------
+// arr.reduce(()=>{},default_value) -> default_value is 0
+const arr = [1, 2, 3, 4, 5]
+let result = arr.reduce((acc, item) => {
+    console.log(`Item : ${item}`)
+    console.log(`Acc : ${acc}`)
+},0)
+
 // *-----------------------------------------------------------------------
+const arr = [1, 2, 3, 4, 5]
+let result = arr.reduce((acc, item) => {
+    console.log(`Item : ${item}`)
+    console.log(`Acc : ${acc}`)
+    return acc;
+},0)
+
 // *-----------------------------------------------------------------------
+const arr = [1, 2, 3, 4, 5]
+let result = arr.reduce((acc, item) => {
+    console.log(`Item : ${item}`)
+    console.log(`Acc : ${acc}`)
+    return acc + 1;
+},0)
 // *-----------------------------------------------------------------------
-// *-----------------------------------------------------------------------
+const arr = [1, 2, 3, 4, 5]
+let result = arr.reduce((acc, item) => {
+    console.log(`Item : ${item}`)
+    console.log(`Acc : ${acc}`)
+    return acc + item;
+},0)
+console.log(result)
--- 2022-June/JavaScript/udemy/Modern JS ES6/tempCodeRunnerFile.js
+++ 2022-June/JavaScript/udemy/Modern JS ES6/tempCodeRunnerFile.js
@@ -1,20 +1,7 @@
-const data = [
-    {
-    name: "Mr Black",
-    exp: 8,
-    hobbies:"Play Game",
-    },
-    {
-    name: "Mr Brown",
-    exp: 5,
-    hobbies:"Cooking"
-    }
-]
-// console.log(data.map((value) => value.name))
-let result = data.map((item) => {
-    return {
-        name: item.name,
-        exp:item.exp
-    }
-})
+const arr = [1, 2, 3, 4, 5]
+let result = arr.reduce((acc, item) => {
+    console.log(`Item : ${item}`)
+    console.log(`Acc : ${acc}`)
+    return acc + item;
+},0)
 console.log(result)
