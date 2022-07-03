260b3156ba85cdc9c20b1a7a40d2959dd53445ab
Nishanth <innocentnishanth@gmail.com>
2022-07-03 09:58:57 +0530

    service and testimonial commpenets removed

--- 2022-June/Portfolio/portfolio/src/App.jsx
+++ 2022-June/Portfolio/portfolio/src/App.jsx
@@ -4,9 +4,7 @@ import Headers from "./components/header/Header";
 import Nav from "./components/nav/Nav";
 import About from "./components/about/About";
 import Experience from "./components/experience/Experience";
-import Services from "./components/services/Services";
 import Portfolio from "./components/portfolio/Portfolio";
-import Testimonials from "./components/testimonials/Testimonials";
 import Contact from "./components/contact/Contact";
 import Footer from "./components/footer/Footer";
 const App = () => {
@@ -16,9 +14,7 @@ const App = () => {
       <Nav />
       <About />
       <Experience />
-      <Services />
       <Portfolio />
-      <Testimonials />
       <Contact />
       <Footer />
     </>
