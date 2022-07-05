256eab4b22c3d502fa456c5bb72a12c4b5a30795
Nishanth <innocentnishanth@gmail.com>
2022-07-05 11:19:11 +0530

    c6 useState eg: count

--- 2022-June/React/English/Dave Gray/src/components/Content.jsx
+++ 2022-June/React/English/Dave Gray/src/components/Content.jsx
@@ -1,62 +1,19 @@
 import React,{useState} from 'react'
 
 const Content = () => {
+  const [count, setCount] = useState(1);
 
-  const [name, setName] = useState("Black");
-  const [count,setCount] = useState(0)
-
-  const handleNameChange = () => {
-        const names = ['Black', "Brown", 'Red', "Blue",'Green']
-        const ran = Math.floor(Math.random() * names.length)
-        setName(names[ran])
-  }
-  {/* Count functions */ }
   const handlePlus = () => {
     setCount(count+1)
   }
   const handleMinus = () => {
-    if (count > 0) {
-      setCount(count - 1)
-    }
-    else {
-      alert("Negative value not allowed")
-    }
+    console.log(count)
+    if (!count > 0) return  alert("Negative value not allowed")
+    setCount(count - 1)
   }
 
-
-  {/* Normal function */}
-  const handleClick = () => {
-    console.log("you clicked")
-  }
-  {/* Passing Value to function */}
-  const handleClick2 = (name) => {
-    console.log(`${name} was cliecked` )
-  }
-  {/* event objects */ }
-  const handleClick3 = (e) => {
-    // console.log(`Event Objects \n ${e}` )
-    // console.log(e)
-    console.log(e.target)
-    console.log(e.target.innerText)
-    e.target.innerText = "after clicked"
-
-  }
   return (
       <main>
-      <p onDoubleClick={handleClick}
-
-      >Hello {name}!
-      </p>
-      <button onClick={handleNameChange} >Name Change</button>
-
-      {/* Normal function call using function reference */}
-      <button onClick={handleClick}>Click It</button>
-
-      {/* Passing Value to function */}
-      <button onClick={() => handleClick2('Mr Black')}>Click It</button>
-
-      {/* Event Object */}
-      <button onClick={(e)=>handleClick3(e)}>Click It</button>
 
       {/* Count */}
       <p> Current Count : {count}</p>
