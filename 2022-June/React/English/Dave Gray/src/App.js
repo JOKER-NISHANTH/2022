dd66d1568aa0e7400c8f64cb3d63fcbcd8216a0b
Nishanth <innocentnishanth@gmail.com>
2022-07-05 09:31:43 +0530

    App & JSX

--- 2022-June/React/English/Dave Gray/src/App.js
+++ 2022-June/React/English/Dave Gray/src/App.js
@@ -1,10 +1,17 @@
 
 import './App.css';
 function App() {
+  const name = "Mr Black a.k.a Nishanth M"
+  const handleNameChange = () => {
+    const names = ['Black', "Brown", 'Red', "Blue"]
+    const ran = Math.floor(Math.random() * 4)
+    return names[ran]
+  }
   return (
-    <div>
-      Hello world
-    </div>
+    <center>
+      Hello world <br></br>
+      <code>{ handleNameChange() }</code>
+    </center>
   );
 }
 
