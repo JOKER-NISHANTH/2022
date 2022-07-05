3cb418c4510d5c015e77c82bfac3ff20c9063a0f
Nishanth <innocentnishanth@gmail.com>
2022-07-05 10:20:35 +0530

    style components

--- 2022-June/React/English/Dave Gray/src/components/Header.jsx
+++ 2022-June/React/English/Dave Gray/src/components/Header.jsx
@@ -1,11 +1,25 @@
 import React from 'react'
 
 const Header = () => {
+  // const headerStyle = {
+  //   backgroundColor: "red",
+  //   color: "#fff"
+  // };
   return (
-      <header>
+    <header
+      //  style={
+      //   {
+      //     backgroundColor: "mediumblue",
+      //     color:"#fff"
+      //   }
+      // }
+
+      // style={headerStyle}
+
+    >
           <h1>Groceries List</h1>
     </header>
   )
 }
 
-export default Header
+export default Header;
