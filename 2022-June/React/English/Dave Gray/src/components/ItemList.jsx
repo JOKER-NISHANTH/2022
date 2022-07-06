90ea5b82ee6028079c2c5e430aea57880c0942b4
Nishanth <innocentnishanth@gmail.com>
2022-07-06 09:57:22 +0530

    notes added c7 1.0

--- 2022-June/React/English/Dave Gray/src/components/ItemList.jsx
+++ 2022-June/React/English/Dave Gray/src/components/ItemList.jsx
@@ -1,35 +1,34 @@
-import React from 'react'
 import { FaTrashAlt } from "react-icons/fa";
 const ItemList = ({items,handleCheck,handleDelete}) => {
   return (
-    <ul >
-    {
-      items.map((item) => {
-        return (
-          <li className="item" key={item.id}>
-            <input
-              type="checkbox"
-              checked={item.checked}
-              onChange={() => handleCheck(item.id)}
-            />
+        <ul >
+        {
+            items.map((item) => {
+                return (
+                <li className="item" key={item.id}>
+                    <input
+                    type="checkbox"
+                    checked={item.checked}
+                    onChange={() => handleCheck(item.id)}
+                    />
 
-            <label
-              style={
-                (item.checked) ? { textDecoration: 'line-through' } : null
-              }
-              onDoubleClick={() => handleCheck(item.id)}
-            >{item.item}</label>
+                    <label
+                    style={
+                        (item.checked) ? { textDecoration: 'line-through' } : null
+                    }
+                    onDoubleClick={() => handleCheck(item.id)}
+                    >{item.item}</label>
 
-            <FaTrashAlt
-              role="button"
-              tabIndex="0"
-              onClick={() => handleDelete(item.id)}
-            />
-          </li>
-        )
-      })
-    }
-  </ul>
+                    <FaTrashAlt
+                    role="button"
+                    tabIndex="0"
+                    onClick={() => handleDelete(item.id)}
+                    />
+                </li>
+                )
+            })
+            }
+        </ul>
   )
 }
 
