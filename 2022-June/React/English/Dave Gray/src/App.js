0aa69e4b00cad6804d252387c36efe56f6cf5f09
Nishanth <innocentnishanth@gmail.com>
2022-07-07 10:04:51 +0530

    c10 Project challenge setup

--- 2022-June/React/English/Dave Gray/src/App.js
+++ 2022-June/React/English/Dave Gray/src/App.js
@@ -1,90 +1,10 @@
-import {useState} from "react"
 import './App.css';
 
-import Header from './components/Header';
-import Content from "./components/Content"
-import Footer from "./components/Footer"
-import AddItem from "./components/AddItem";
-import SearchItem from "./components/SearchItem";
-
 function App() {
-  // const initialState = [{id: 1, checked: true,item:"Apple"},
-  // {id: 2, checked: false,item:"Orange"},
-  //   { id: 3, checked: false, item: "Mango" },]
-
-  const initialState = JSON.parse((localStorage.getItem('shoppinglist')) )
-  const [items, setItems] = useState(initialState);
-
-const [newItem, setNewItem] = useState('')
-
-const setAndSaveItems = (listItems) => {
-  setItems(listItems)
-  localStorage.setItem("shoppinglist", JSON.stringify(listItems));
-}
-
-const handleCheck = (id) => {
-  console.log(`Key ID : ${id}`)
-  const listItems = items.map(
-    (item) =>  item.id === id ? { ...item, checked: !item.checked }:item
-  )
-  setAndSaveItems(listItems)
-}
-
-const handleDelete = (id) => {
-  // console.log(id)
-  const listItems = items.filter((item) => (item.id !== id))
-  setAndSaveItems(listItems)
-}
-
-const addItem = (item) => {
-  const id = items.length ? items[items.length - 1].id + 1 : 1;
-  const myNewItem = { id, checked: false, item };
-  const listItems = [...items, myNewItem];
-  setAndSaveItems(listItems)
-}
-const handleSubmit = (e) => {
-  e.preventDefault();
-  if (!newItem) return;
-  console.log(newItem)
-  addItem(newItem)
-  setNewItem('')
-}
-
-  const [search, setSearch] = useState('')
-
   return (
-    <div className='App'>
-      <Header title="Todo List" />
-      <AddItem
-        newItem= {newItem}
-        setNewItem={setNewItem}
-        handleSubmit={handleSubmit}
-      />
+    <div className='App' >
 
-        <SearchItem
-        search={search}
-        setSearch={setSearch}
-
-      />
-
-      <Content
-        items={items.filter(
-          item =>
-            (
-              (item.item).toLowerCase()
-            ).includes(search)
-        )}
-        setItems={setItems}
-        handleCheck={handleCheck}
-        handleDelete={handleDelete}
-
-      />
-
-      <Footer len={items.length} />
-
-
-    </div>
+      </div>
   );
 }
-
 export default App;
--- 2022-June/React/English/Dave Gray/src/utils/setAndSaveItems.js
+++
@@ -1,4 +0,0 @@
-export  default function setAndSaveItems (listItems,setItems)  {
-    setItems(listItems)
-    localStorage.setItem("shoppinglist", JSON.stringify(listItems));
-}
