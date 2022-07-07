b4e938f7c3cf9e124cea0ea4878177815c004921
Nishanth <innocentnishanth@gmail.com>
2022-07-07 13:47:38 +0530

    c14 complete CRUD using json-server

---
+++ 2022-June/React/English/Dave Gray/src/utils/apiRequest.js
@@ -0,0 +1,15 @@
+const apiRequest =
+    async (url = '', optionsObject = null, errorMessage = null) =>
+    {
+        try {
+            const response = await fetch(url, optionsObject);
+            if (!response.ok) throw Error("Please reload the application");
+
+        } catch (error) {
+            errorMessage = error.message;
+        } finally {
+            return errorMessage;
+        }
+    }
+
+export default apiRequest
