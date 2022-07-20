bab04b6c0ff5b2a3d0888a9fca60fc61ba32c69e
Nishanth <innocentnishanth@gmail.com>
2022-07-20 19:01:13 +0530

    initial setup for webdevarmy

--- 2022-June/React/Tamil/Paylagam/src/App.js
+++ 2022-June/React/Tamil/Paylagam/src/App.js
@@ -1,11 +1,16 @@
 
 import './App.css';
+// import Counter from "./components/Counter"
+import Table from "./components/Table"
 
 function App() {
 
   return (
     <div className="App">
-      <h1>Welcome to learn <br />React js </h1>
+      {/* <h1>Welcome to learn <br />React js </h1> */}
+      {/* <Counter /> */}
+
+      <Table />
 
     </div>
   );
