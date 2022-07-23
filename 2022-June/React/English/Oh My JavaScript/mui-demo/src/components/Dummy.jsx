4e8a60813409f9e70bd9136f21f2a7283d2e305f
Nishanth <innocentnishanth@gmail.com>
2022-07-23 19:59:48 +0530

    Container

--- 2022-June/React/English/Oh My JavaScript/mui-demo/src/components/Dummy.jsx
+++ 2022-June/React/English/Oh My JavaScript/mui-demo/src/components/Dummy.jsx
@@ -1,22 +1,18 @@
 import React from 'react'
+import Container from '@mui/material/Container';
 import Box from '@mui/material/Box';
 import Typography from '@mui/material/Typography';
 
 const Dummy = () => {
   return (
-      <div>
-      <Box sx={
-        {
-          maxWidth: 500,
-          m: 10,
-          border:"1px solid black"
-        }
-      }>
-        <Typography variant="h2" component="h1" gutterBottom noWrap>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae quidem error minima?
-          </Typography >
-        </Box>
-    </div>
+    <>
+      <Container maxWidth="md">
+        <Box sx={{
+          bgcolor: "red",
+          height:'100vh'
+        }} />
+      </Container>
+    </>
   )
 }
 
