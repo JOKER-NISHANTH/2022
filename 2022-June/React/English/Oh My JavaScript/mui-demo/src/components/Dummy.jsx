d3e77db9ee151fb68008aaf9abab26a65a40c68c
Nishanth <innocentnishanth@gmail.com>
2022-07-23 19:43:55 +0530

    Typography

--- 2022-June/React/English/Oh My JavaScript/mui-demo/src/components/Dummy.jsx
+++ 2022-June/React/English/Oh My JavaScript/mui-demo/src/components/Dummy.jsx
@@ -1,21 +1,20 @@
 import React from 'react'
 import Box from '@mui/material/Box';
-import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
+
 const Dummy = () => {
   return (
       <div>
       <Box sx={
         {
+          maxWidth: 500,
           m: 10,
-          width: 300,
-          height: 300,
-          backgroundColor: "blue",
-          "&:hover": {
-            backgroundColor: "skyblue",
-          }
+          border:"1px solid black"
         }
       }>
-
+        <Typography variant="h2" component="h1" gutterBottom noWrap>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae quidem error minima?
+          </Typography >
         </Box>
     </div>
   )
