5dd46eb200ae7715da30e6e07339dae3cb541701
Nishanth <innocentnishanth@gmail.com>
2022-08-10 06:47:41 +0530

    initial instagram clone

---
+++ 2022-June/Bootcamps/DuoLearn/insta-clone/pages/index.js
@@ -0,0 +1,8 @@
+import {useState} from "react"
+export default function Home() {
+  const [first, setfirst] = useState(0)
+  return (
+    <>
+    </>
+  )
+}
---
+++ 2022-June/Bootcamps/DuoLearn/insta-clone/pages/test.js
@@ -0,0 +1,5 @@
+const Test = () => {
+    return <>Test Data</>
+}
+
+export default Test;
