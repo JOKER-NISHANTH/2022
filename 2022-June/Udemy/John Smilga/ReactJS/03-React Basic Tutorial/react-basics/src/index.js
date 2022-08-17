2076abfe915f141942c377d3f8f0aa01d4f3da3b
Nishanth <innocentnishanth@gmail.com>
2022-08-17 07:12:31 +0530

    upto 20

---
+++ 2022-June/Udemy/John Smilga/ReactJS/03-React Basic Tutorial/react-basics/src/index.js
@@ -0,0 +1,36 @@
+import React from "react";
+import ReactDom from "react-dom";
+
+/*
+    Mini Book Project
+*/
+
+const BookList = () => {
+  return (
+    <section>
+      <Book />
+    </section>
+  );
+};
+
+const Book = () => {
+  return (
+    <article>
+      <Image />
+      <Title />
+      <Author />
+      <Price />
+    </article>
+  );
+};
+const Image = () => (
+  <img
+    src="https://images-na.ssl-images-amazon.com/images/I/51-nXsSRfZL._SY264_BO1,204,203,200_QL40_FMwebp_.jpg"
+    alt="Atomic-Habits"
+  />
+);
+
+const Title = () => <h1>Atomic Habits: The life-changing million</h1>;
+const Author = () => <h3>James Clear</h3>;
+const Price = () => <h6>$5</h6>;
+ReactDom.render(<BookList />, document.getElementById("root"));
