c73c563641e275ed9b59968056e928fbf61b1f55
Nishanth <innocentnishanth@gmail.com>
2022-08-17 20:50:01 +0530

    react basic complete

--- 2022-June/Udemy/John Smilga/ReactJS/03-React Basic Tutorial/react-basics/src/index.js
+++ 2022-June/Udemy/John Smilga/ReactJS/03-React Basic Tutorial/react-basics/src/index.js
@@ -1,35 +1,9 @@
 import React from "react";
 import ReactDom from "react-dom";
 import "./index.css";
-/*
-    Attribute
-    EventHandler
-*/
-
+import { Book } from "./Book";
+import { books } from "./books";
 // setup vars
-const books = [
-  {
-    id: 1,
-    author: "James Clear",
-    title: "Atomic Habits: The life-changing million",
-    img: "https://images-na.ssl-images-amazon.com/images/I/51-nXsSRfZL._SY264_BO1,204,203,200_QL40_FMwebp_.jpg",
-    altImg: "Atomic-Habits",
-  },
-  {
-    id: 2,
-    author: "George Matthew Adams",
-    title: "You Can",
-    img: "https://images-eu.ssl-images-amazon.com/images/I/813uPMOnskL._AC_UL604_SR604,400_.jpg",
-    altImg: "You Can",
-  },
-  {
-    id: 3,
-    author: "Paulo Coelho",
-    title: "The Alchemist",
-    img: "https://images-eu.ssl-images-amazon.com/images/I/71aFt4+OTOL._AC_UL604_SR604,400_.jpg",
-    altImg: "The Alchemist",
-  },
-];
 
 const BookList = () => {
   return (
@@ -42,29 +16,4 @@ const BookList = () => {
   );
 };
 
-const Book = ({ title, author, img, altImg }) => {
-  // attribute and eventHandler
-  // onClick
-  // onMouseOver
-  const clickHandler = () => {
-    alert("Click");
-  };
-  const complexHandler = (author) => {
-    alert(author);
-  };
-  return (
-    <article className="book">
-      <img src={img} alt={altImg} />
-      <h1 onClick={() => alert(title)}>{title}</h1>
-      <h4>{author}</h4>
-      <button type="button" onClick={clickHandler}>
-        reference
-      </button>
-
-      <button type="button" onClick={() => complexHandler(author)}>
-        more complex
-      </button>
-    </article>
-  );
-};
 ReactDom.render(<BookList />, document.getElementById("root"));
