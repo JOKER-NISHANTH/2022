71dc6a96ef64b07f4b4da29641ae3f9958e502b3
Nishanth <innocentnishanth@gmail.com>
2022-08-19 05:25:46 +0530

    rules of hooks

--- 2022-June/Udemy/John Smilga/ReactJS/04-React Hooks and Advanced Topic/react-advanced-v18/src/App.js
+++ 2022-June/Udemy/John Smilga/ReactJS/04-React Hooks and Advanced Topic/react-advanced-v18/src/App.js
@@ -1,6 +1,12 @@
 import React from "react";
 // import Setup from "./tutorial/1-useState/setup/1-error-example";
-import Setup from "./tutorial/1-useState/setup/2-useState-basics";
+// import Setup from "./tutorial/1-useState/setup/2-useState-basics";
+import Setup from "./tutorial/1-useState/setup/3-useState-array";
+/*
+  component name must be uppercase
+  must be in the function/component body
+  cannot call Conditionally
+*/
 function App() {
   return (
     <div className="container">
