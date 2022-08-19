4bd5687cb50d5cf08254378d702d4f5ed6aa69b0
Nishanth <innocentnishanth@gmail.com>
2022-08-19 05:58:35 +0530

    useState with Object

--- 2022-June/Udemy/John Smilga/ReactJS/04-React Hooks and Advanced Topic/react-advanced-v18/src/App.js
+++ 2022-June/Udemy/John Smilga/ReactJS/04-React Hooks and Advanced Topic/react-advanced-v18/src/App.js
@@ -1,7 +1,8 @@
 import React from "react";
 // import Setup from "./tutorial/1-useState/setup/1-error-example";
 // import Setup from "./tutorial/1-useState/setup/2-useState-basics";
-import Setup from "./tutorial/1-useState/setup/3-useState-array";
+// import Setup from "./tutorial/1-useState/setup/3-useState-array";
+import Setup from "./tutorial/1-useState/setup/4-useState-object";
 /*
   component name must be uppercase
   must be in the function/component body
--- 2022-June/Udemy/John Smilga/ReactJS/04-React Hooks and Advanced Topic/react-advanced-v18/src/tutorial/1-useState/setup/4-useState-object.js
+++ 2022-June/Udemy/John Smilga/ReactJS/04-React Hooks and Advanced Topic/react-advanced-v18/src/tutorial/1-useState/setup/4-useState-object.js
@@ -1,7 +1,30 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
 const UseStateObject = () => {
-  return <h2>useState object example</h2>;
+  const data = {
+    id: 1,
+    name: "Mr Black",
+    age: 23,
+    message: "random message",
+    job: "MERN Stack Dev",
+  };
+  const [person, setPerson] = useState(data);
+  const changeJob = () => {
+    setPerson({
+      ...person,
+      job: "Ethical Hacker",
+    });
+  };
+  return (
+    <>
+      <h3> Name : {person.name}</h3>
+      <h3> Age : {person.age}</h3>
+      <h3> Job : {person.job}</h3>
+      <button onClick={changeJob} type="button" className="btn">
+        change
+      </button>
+    </>
+  );
 };
 
 export default UseStateObject;
