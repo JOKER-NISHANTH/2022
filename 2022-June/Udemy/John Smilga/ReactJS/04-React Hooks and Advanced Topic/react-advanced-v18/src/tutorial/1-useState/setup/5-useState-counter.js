41141991fa4b15493d7417d6b17a52db2597569f
Nishanth <innocentnishanth@gmail.com>
2022-08-19 09:21:49 +0530

    useState async

--- 2022-June/Udemy/John Smilga/ReactJS/04-React Hooks and Advanced Topic/react-advanced-v18/src/tutorial/1-useState/setup/5-useState-counter.js
+++ 2022-June/Udemy/John Smilga/ReactJS/04-React Hooks and Advanced Topic/react-advanced-v18/src/tutorial/1-useState/setup/5-useState-counter.js
@@ -5,22 +5,53 @@ const UseStateCounter = () => {
   const handleReset = () => {
     setValue(0);
   };
+
+  {
+    /*
+    Functional Update form
+    in hook functions are sync
+     */
+  }
+  const handleAsyncIncrease = () => {
+    setTimeout(() => {
+      // setValue(value + 1);
+      setValue((prevState) => prevState + 1);
+    }, 2000);
+  };
   return (
-    <section style={{ margin: "4rem 0" }}>
-      <h2>regular counter</h2>
-      <h1>{value}</h1>
-      <button className="btn" type="button" onClick={() => setValue(value - 1)}>
-        decrease
-      </button>
+    <>
+      <section style={{ margin: "4rem 0" }}>
+        <h2>regular counter</h2>
+        <h1>{value}</h1>
+        <button
+          className="btn"
+          type="button"
+          onClick={() => setValue(value - 1)}
+        >
+          decrease
+        </button>
 
-      <button className="btn" type="button" onClick={handleReset}>
-        reset
-      </button>
+        <button className="btn" type="button" onClick={handleReset}>
+          reset
+        </button>
 
-      <button className="btn" type="button" onClick={() => setValue(value + 1)}>
-        increase
-      </button>
-    </section>
+        <button
+          className="btn"
+          type="button"
+          onClick={() => setValue(value + 1)}
+        >
+          increase
+        </button>
+      </section>
+      {/*Functional Update form */}
+      <section style={{ margin: "4rem 0" }}>
+        <h2>More Complex counter</h2>
+        <h1>{value}</h1>
+        <button className="btn" type="button" onClick={handleAsyncIncrease}>
+          increase later
+        </button>
+      </section>
+    </>
   );
 };
 
