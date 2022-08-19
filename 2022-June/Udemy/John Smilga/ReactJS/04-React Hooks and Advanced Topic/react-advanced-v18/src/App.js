ad43f4627fec9837f9a4c5b1315e4959ecc025ff
Nishanth <innocentnishanth@gmail.com>
2022-08-19 09:33:49 +0530

    basic useEffect

--- 2022-June/Udemy/John Smilga/ReactJS/04-React Hooks and Advanced Topic/react-advanced-v18/src/App.js
+++ 2022-June/Udemy/John Smilga/ReactJS/04-React Hooks and Advanced Topic/react-advanced-v18/src/App.js
@@ -3,7 +3,10 @@ import React from "react";
 // import Setup from "./tutorial/1-useState/setup/2-useState-basics";
 // import Setup from "./tutorial/1-useState/setup/3-useState-array";
 // import Setup from "./tutorial/1-useState/setup/4-useState-object";
-import Setup from "./tutorial/1-useState/setup/5-useState-counter";
+// import Setup from "./tutorial/1-useState/setup/5-useState-counter";
+
+import Setup from "./tutorial/2-useEffect/setup/1-useEffect-basics";
+
 /*
   component name must be uppercase
   must be in the function/component body
--- 2022-June/Udemy/John Smilga/ReactJS/04-React Hooks and Advanced Topic/react-advanced-v18/src/tutorial/2-useEffect/setup/1-useEffect-basics.js
+++ 2022-June/Udemy/John Smilga/ReactJS/04-React Hooks and Advanced Topic/react-advanced-v18/src/tutorial/2-useEffect/setup/1-useEffect-basics.js
@@ -1,9 +1,22 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
 // by default runs after every re-render
 // cleanup function
 // second parameter
 const UseEffectBasics = () => {
-  return <h2>useEffect Basics</h2>;
+  const [value, setValue] = useState(1);
+  useEffect(() => {
+    console.log("useEffect");
+  });
+  console.log("Render Component");
+  return (
+    <>
+      <h2>useEffect Basics</h2>
+      <h1>{value}</h1>
+      <button className="btn" type="button" onClick={() => setValue(value + 1)}>
+        increase
+      </button>
+    </>
+  );
 };
 
 export default UseEffectBasics;
