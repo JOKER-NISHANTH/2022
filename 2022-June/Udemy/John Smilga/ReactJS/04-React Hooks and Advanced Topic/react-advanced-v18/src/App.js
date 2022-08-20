52f6ee65cd5acf383ab33fcb1ce650a7bdd34e37
Nishanth <innocentnishanth@gmail.com>
2022-08-20 08:13:28 +0530

    multiple return with fetch eg

--- 2022-June/Udemy/John Smilga/ReactJS/04-React Hooks and Advanced Topic/react-advanced-v18/src/App.js
+++ 2022-June/Udemy/John Smilga/ReactJS/04-React Hooks and Advanced Topic/react-advanced-v18/src/App.js
@@ -14,6 +14,7 @@ import React from "react";
 
 // conditional Rendering
 import Setup from "./tutorial/3-conditional-rendering/setup/1-multiple-returns";
+// import Setup from "./tutorial/3-conditional-rendering/setup/2-short-circuit";
 
 /*
   component name must be uppercase
