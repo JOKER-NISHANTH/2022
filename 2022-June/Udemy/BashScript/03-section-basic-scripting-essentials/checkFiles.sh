964f7555b110236703b9edf0d2b32b0f0a1a769c
Nishanth <innocentnishanth@gmail.com>
2022-08-25 22:35:34 +0530

    check files is exits or not -f

---
+++ 2022-June/Udemy/BashScript/03-section-basic-scripting-essentials/checkFiles.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+if [ -f /mnt/d/videoTutorialFiles/2022/2022-June/Udemy/BashScript/03-section-basic-scripting-essentials/testFile ]
+	then
+		echo "Variable files is exits"
+
+	else
+		echo "Variable files does not exits"
+fi
