5263d733f16ae81b482dea182cb1d92b26c6befa
Nishanth <innocentnishanth@gmail.com>
2022-08-25 22:35:15 +0530

    check directory -d

---
+++ 2022-June/Udemy/BashScript/03-section-basic-scripting-essentials/checkDirectory.sh
@@ -0,0 +1,10 @@
+
+#!/bin/bash
+
+if [ -d /mnt/d/videoTutorialFiles/2022/2022-June/Udemy/BashScript/03-section-basic-scripting-essentials ]
+        then
+                echo "Directory is exits"
+
+        else
+                echo "Directory files does not exits"
+fi
