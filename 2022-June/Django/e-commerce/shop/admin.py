f1a7e5aee2b2ff6dde267aae240adb1883624993
Nishanth <innocentnishanth@gmail.com>
2022-09-03 07:26:53 +0530

    model registered

--- 2022-June/Django/e-commerce/shop/admin.py
+++ 2022-June/Django/e-commerce/shop/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
 
 # Register your models here.
+
+# from shop import models
+from .models import *
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display= ("name","image","description")
+
+
+admin.site.register(Category,CategoryAdmin)
+admin.site.register(Product)
