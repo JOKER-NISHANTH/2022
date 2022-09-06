3bc920420c0886a4cc28569e194eaea41e9c4c44
Nishanth <innocentnishanth@gmail.com>
2022-09-06 23:24:55 +0530

    django project incompleted

--- 2022-June/Django/e-commerce/shop/models.py
+++ 2022-June/Django/e-commerce/shop/models.py
@@ -1,5 +1,6 @@
-from email.policy import default
+from itertools import product
 from django.db import models
+from django.contrib.auth.models import User
 import datetime
 import os
 # Create your models here.
@@ -37,3 +38,36 @@ class Product(models.Model):
 
     def __str__(self):
         return self.name
+
+
+
+class Product(models.Model):
+  category=models.ForeignKey(Catagory,on_delete=models.CASCADE)
+  name=models.CharField(max_length=150,null=False,blank=False)
+  vendor=models.CharField(max_length=150,null=False,blank=False)
+  product_image=models.ImageField(upload_to=getFileName,null=True,blank=True)
+  quantity=models.IntegerField(null=False,blank=False)
+  original_price=models.FloatField(null=False,blank=False)
+  selling_price=models.FloatField(null=False,blank=False)
+  description=models.TextField(max_length=500,null=False,blank=False)
+  status=models.BooleanField(default=False,help_text="0-show,1-Hidden")
+  trending=models.BooleanField(default=False,help_text="0-default,1-Trending")
+  created_at=models.DateTimeField(auto_now_add=True)
+
+  def __str__(self) :
+    return self.name
+
+class Cart(models.Model):
+  user=models.ForeignKey(User,on_delete=models.CASCADE)
+  product=models.ForeignKey(Product,on_delete=models.CASCADE)
+  product_qty=models.IntegerField(null=False,blank=False)
+  created_at=models.DateTimeField(auto_now_add=True)
+
+  @property
+  def total_cost(self):
+    return self.product_qty*self.product.selling_price
+
+class Favourite(models.Model):
+	user=models.ForeignKey(User,on_delete=models.CASCADE)
+	product=models.ForeignKey(Product,on_delete=models.CASCADE)
+	created_at=models.DateTimeField(auto_now_add=True)
