396c8ac1cc96b3258126c669a592160200d48003
Nishanth <innocentnishanth@gmail.com>
2022-09-07 18:52:02 +0530

    v-60 add item to the list , create id using Date function

--- 2022-June/Udemy/John Smilga/ReactJS/04-React Hooks and Advanced Topic/react-advanced-v18/src/tutorial/4-forms/setup/1-controlled-inputs.js
+++ 2022-June/Udemy/John Smilga/ReactJS/04-React Hooks and Advanced Topic/react-advanced-v18/src/tutorial/4-forms/setup/1-controlled-inputs.js
@@ -8,10 +8,30 @@ import React, { useState } from "react";
 const ControlledInputs = () => {
   const [firstName, setFirstName] = useState("");
   const [email, setEmail] = useState("");
-
+  const [people, setPeople] = useState([]);
   const handleSubmit = (e) => {
     e.preventDefault();
     // console.log("form submitted");
+    // console.log(firstName,email)
+
+    if (firstName && email) {
+      // console.log("form submitted");
+      const person = {
+        id: new Date().getTime().toString(),
+        firstName,
+        email,
+      };
+      // console.log(person);
+      setPeople((prePeople) => {
+        return [...prePeople, person];
+      });
+
+      // again empty the firstName and email
+      setFirstName("");
+      setEmail("");
+    } else {
+      console.log("empty values");
+    }
   };
 
   return (
@@ -48,6 +68,18 @@ const ControlledInputs = () => {
             add person
           </button>
         </form>
+      </article>
+
+      <article>
+        {people.map((person) => {
+          const { id, firstName, email } = person;
+          return (
+            <div className="item" key={id}>
+              <h4>{firstName}</h4>
+              <h4>{email}</h4>
+            </div>
+          );
+        })}
       </article>
     </>
   );
