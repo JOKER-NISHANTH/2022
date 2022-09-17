890fbdc03b508529418e538aa7a2e938df516187
Nishanth <innocentnishanth@gmail.com>
2022-09-17 18:40:54 +0530

    v-1 RE intro

---
+++ 2022-June/Youtube/LeelaWebDev/RegularExpression/01-intro.js
@@ -0,0 +1,7 @@
+// 1. Regular express concept was first developed in 1950 bt mathematician Stephen Keene
+// 2. It was first developed with the name of Regular language
+// 3. GREP stands for the Global Search for Regular Expression and Print matching
+// 4. The first version of regular expression was used in PERL in 1980 .
+// 5. This was the first time used in the programming languages
+// 6. In 1997 Philip Hazel has developed PCRE for use in many modern tools
+// 7. PCRE stands for Perl compatible for Regular Expression
